fd67d5d55fb0d045c654756d9b453144c3a96b3e
whackAF <76838932+Blangoat@users.noreply.github.com>
2021-05-18 19:04:17 +0200

    mbeWK

--- TrashMaster/Frames/Overblik.xaml.cs
+++ TrashMaster/Frames/Overblik.xaml.cs
@@ -163,8 +163,8 @@ namespace TrashMaster.Frames
         //Formater Decimal til sepperering med punktum og to decimaler
         private void OnAutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
         {
-            //if (e.PropertyType == typeof(DateTime))
-            //    (e.Column as DataGridTextColumn).Binding.StringFormat = "yyyy-MM-dd HH:mm:ss";
+            if (e.PropertyType == typeof(DateTime))
+                (e.Column as DataGridTextColumn).Binding.StringFormat = "M/dd/yyyy hh:mm:ss tt";
 
             if (e.PropertyType == typeof(Decimal))
                 (e.Column as DataGridTextColumn).Binding.StringFormat = "{0:0.00}";
